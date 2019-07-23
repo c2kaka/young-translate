8f65f25351c5c5285431c68e11557a735634c214
“c2kaka <c2kaka@126.com>
2019-07-23 13:37:10 +0800

    基本完成小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -40,41 +40,7 @@ Page({
     if (this.data.curLang.lang !== app.globalData.curLang.lang){
       this.setData({curLang:app.globalData.curLang});
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+    this.setData({ showCloseIcon: this.data.queryContent.length > 0 });
   },
 
   /**
